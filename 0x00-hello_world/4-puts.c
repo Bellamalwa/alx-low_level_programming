f07ef77648bd5155a7846f25e4c6c2731204af17
Bellamalwa <duchybella@gmail.com>
2023-03-16 13:40:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - Entry point
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-    printf("Programming is like building a multilingual puzzle\n");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
