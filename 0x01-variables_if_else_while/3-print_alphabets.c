242ac743a317f5aab2466810cd9876c7d0706d1b
Bellamalwa <duchybella@gmail.com>
2023-03-17 12:24:57 -0700

    3-print_alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-  *main - Entry point
+  *main- Entry point
   *
-  *Return: - Always 0 (success)
+  *Return: - Always 0 (sucess)
   */
 
 int main(void)
@@ -15,6 +15,14 @@ while (letter <= 'z')
 putchar(letter);
 letter++;
 }
+letter = 'A';
+
+while (letter <= 'Z')
+{
+putchar(letter);
+letter++;
+}
+
 putchar('\n');
 
 return (0);
