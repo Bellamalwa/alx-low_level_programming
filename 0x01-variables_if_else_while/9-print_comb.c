ebe52a10efd2987a9d3d758d48384dbdc432701e
Bellamalwa <duchybella@gmail.com>
2023-03-17 13:53:50 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,14 @@
 
 int main(void)
 {
-int i, j;
-for (i = 0; i < 10; i++)
+int a;
+for (a = 0; a < 10; a++)
 {
-for (j = i; j < 10; j++)
-{
-putchar(i + '0');
-putchar(',');
-putchar(' ');
-putchar(j + '0');
-if (j != 9)
+putchar(a + '0');
+if (a < 9)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
