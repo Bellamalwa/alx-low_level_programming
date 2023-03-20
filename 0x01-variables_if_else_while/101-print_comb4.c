fd8870379274ebbdba6d3ad1531b54451804eb4a
Bellamalwa <duchybella@gmail.com>
2023-03-20 12:22:00 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,22 +10,27 @@
 
 int main(void)
 {
-int i, j, k;
-for (int i = 0; i <= 7; i++)
+int i;
+int j;
+int k;
+
+for (i = 0; i < 10; i++)
 {
-for (int j = i + 1; j <= 8; j++)
+for (j = 1; j < 10; j++)
 {
-for (int k = j + 1; k <= 9; k++)
+for (k = 2; k < 10; k++)
+{
+if (i < j && j < k)
 {
 putchar(i + '0');
 putchar(j + '0');
 putchar(k + '0');
-if (i != 7 || j != 8 || k != 9)
 putchar(',');
 putchar(' ');
 }
 }
 }
 }
+putchar('\n');
 return (0);
 }
