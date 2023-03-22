2411c75d9ff26255e8080749288c8ed6da9326cf
Bellamalwa <duchybella@gmail.com>
2023-03-21 18:03:55 -0700

    task8,9,11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - to print numbers
  *
@@ -37,9 +36,11 @@ else if (n > 98)
 for (i = n ; i >= 98 ; i--)
 {
 printf("%d", i);
+if (i != 98)
+printf(", ");
 }
 printf("\n");
 }
 else if (n == 98)
-printf("%d, ", n);
+printf("%d", n);
 }
