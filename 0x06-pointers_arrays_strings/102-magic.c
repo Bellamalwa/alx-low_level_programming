01651e4015b8329c0a8f58ed01fa1f07cbea8fce
Bellamalwa <duchybella@gmail.com>
2023-03-30 10:46:02 -0700

    task10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-int n;
-int a[5];
+int a[] = { 0, 1, 2, 3, 4 };
 int *p;
 
-a[2] = 1024;
-p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-*(p + 5) = 98;
-  /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
+p = &a[2];
+
+_putchar('a[2] = %d', *(p));
+_putchar('%c', '\n');
 return (0);
 }
+
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,82 +1,70 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+ * infinite_add - check the code
+ * @n1: variable 1
+ * @n2: variable 2
+ * @r: variable 3
+ * @size_r: variable 4
+ *Return: pointer to result 
  */
-
-void rev_string(char *n)
-{
-int i = 0;
-int j = 0;
-char temp;
-
-while (*(n + i) != '\0')
-{
-i++;
-}
-i--;
-
-for (j = 0; j < i; j++, i--)
-
-temp = *(n + j);
-*(n + j) = *(n + i);
-*(n + i) = temp;
-}
-}
-
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int main(void);
+int len1 = strlen(n1);
+int len2 = strlen(n2);
+int carry = 0;
+int sum;
+int i, j;
+
+if (len1 + 1 > size_r || len2 + 1 > size_r)
 {
-int overflow = 0, i = 0, j = 0, digits = 0;
-int val1 = 0, val2 = 0, temp_tot = 0;
+return (0);
 }
 
-while (*(n1 + i) != '\0')
-i++;
-while (*(n2 + j) != '\0')
-j++;
-i--;
-j--;
-if (j >= size_r || i >= size_r)
-return (0);
-while (j >= 0 || i >= 0 || overflow == 1)
+for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--)
 {
-if (i < 0)
-val1 = 0;
-else
-val1 = *(n1 + i) -'0';
-if (j < 0)
-val2 = 0;
-else
-val2 = *(n2 + j) -'0';
-temp_tot = val1 + val2 + overflow;
-if (temp_tot >= 10)
-overflow = 1;
-else
-overflow = 0;
-if (digits >= (size_r - 1))
-return (0);
-*(r + digits) = (temp_tot % 10) + '0';
-digits++;
-j--;
-i--;
+sum = carry;
+if (i >= 0)
+{
+sum += n1[i] - '0';
 }
-if (digits == size_r)
+if (j >= 0)
+{
+sum += n2[j] - '0';
+}
+if (sum > 9)
+{
+carry = 1;
+sum -= 10;
+}
+else
+{
+carry = 0;
+}
+r[len1 + len2 - i - j - 2] = sum + '0';
+}
+
+
+if (carry)
+{
+if (len1 + len2 >= size_r)
+{
 return (0);
-*(r + digits) = '\0';
-rev_string(r);
+}
+r[len1 + len2 - i - j - 2] = '1';
+}
+
+for (i = 0, j = len1 + len2 - 2; i < j; i++, j--)
+{
+char tmp = r[i];
+r[i] = r[j];
+r[j] = tmp;
+}
+
+r[len1 + len2 - 1] = '\0';
+
 return (r);
 }
 
