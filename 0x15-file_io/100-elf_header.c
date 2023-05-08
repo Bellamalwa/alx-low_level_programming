00d6f7282c1d1063804e3e3268fa7ad6a0b1f7f6
Bellamalwa <duchybella@gmail.com>
2023-05-08 16:58:54 -0700

    task5

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,103 +1,42 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 /**
- * elf_header - displays the information contained in the ELF header
- * at the start of an ELF file
- * @filename: the name of the ELF file
- * Return: 0 on success, 98 on failure
+ * check_elf - Checks if a file is an ELF file.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
+ * Description: If the file is not an ELF file - exit code 98.
  */
-int elf_header(char *filename)
+void check_elf(unsigned char *e_ident)
 {
-    int fd, i;
-    Elf64_Ehdr *header;
-    char magic[16] = {0x7f, 'E', 'L', 'F', 0};
+	int index;
 
-    /* Open the ELF file */
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        fprintf(stderr, "Error: Cannot open file %s\n", filename);
-        return 98;
-    }
-
-    /* Read the ELF header */
-    header = malloc(sizeof(Elf64_Ehdr));
-    if (!header)
-    {
-        fprintf(stderr, "Error: Cannot allocate memory\n");
-        return 98;
-    }
-    if (read(fd, header, sizeof(Elf64_Ehdr)) != sizeof(Elf64_Ehdr))
-    {
-        fprintf(stderr, "Error: Cannot read ELF header\n");
-        free(header);
-        return 98;
-    }
-
-    /* Check if the file is an ELF file */
-    size_t magic_size = sizeof(magic);
-    for (size_t i = 0; i < magic_size; i++)
-    {
-        if (header->e_ident[i] != magic[i])
-        {
-            fprintf(stderr, "Error: %s is not an ELF file\n", filename);
-            free(header);
-            return 98;
-        }
-    }
-
-    /* Print the ELF header information */
-    printf("Magic:   ");
-    for (i = 0; i < EI_NIDENT; i++)
-        printf("%02x ", header->e_ident[i]);
-    printf("\n");
-    printf("Class:                             %s\n",
-           header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
-           header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" :
-           header->e_ident[EI_CLASS] == ELFCLASSNONE ? "none" : "unknown");
-    printf("Data:                              %s\n",
-           header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" :
-           header->e_ident[EI_DATA] == ELFDATA2MSB ? "2's complement, big endian" :
-           header->e_ident[EI_DATA] == ELFDATANONE ? "none" : "unknown");
-    printf("Version:                           %d%s\n",
-           header->e_ident[EI_VERSION],
-           header->e_ident[EI_VERSION] == EV_CURRENT ? " (current)" : "");
-    printf("OS/ABI:                            %s\n",
-           header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" :
-           header->e_ident[EI_OSABI] == ELFOSABI_HPUX ? "UNIX - HP-UX" :
-           header->e_ident[EI_OSABI] == ELFOSABI_NETBSD ? "UNIX - NetBSD" :
-           header->e_ident[EI_OSABI] == ELFOSABI_LINUX ? "UNIX - GNU" :
-           header->e_ident[EI_OSABI] == ELFOSABI_SOLARIS ? "UNIX - Solaris" :
-           header->e_ident[EI_OSABI] == ELFOSABI_IRIX ? "UNIX - IRIX" :
-           header->e_ident[EI_OSABI] == ELFOSABI_FREEBSD ? "UNIX - FreeBSD" :
-           header->e_ident[EI_OSABI] == ELFOSABI_TRU64 ? "UNIX - TRU64" :
-           header->e_ident[EI_OSABI] == ELFOSABI_ARM ? "ARM" :
-           header->e_ident[EI_OSABI] == ELFOSABI_STANDALONE ? "Standalone App" : "UNIX - unknown");
-    printf("ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
-    printf("Type:                              %s\n",
-           header->e_type == ET_NONE ? "NONE (None)" :
-           header->e_type == ET_REL ? "REL (Relocatable file)" :
-           header->e_type == ET_EXEC ? "EXEC (Executable file)" :
-           header->e_type == ET_DYN ? "DYN (Shared object file)" :
-           header->e_type == ET_CORE ? "CORE (Core file)" :
-           "unknown");
-    printf("Entry point address:               0x%lx\n", (unsigned long)header->e_entry);
-    printf("Start of program headers:          %lu (bytes into file)\n", (unsigned long)header->e_phoff);
-    printf("Start of section headers:          %lu (bytes into file)\n", (unsigned long)header->e_shoff);
-    printf("Flags:                             0x%x\n", header->e_flags);
-    printf("Size of this header:               %d (bytes)\n", header->e_ehsize);
-    printf("Size of program headers:           %d (bytes)\n", header->e_phentsize);
-    printf("Number of program headers:         %d\n", header->e_phnum);
-    printf("Size of section headers:           %d (bytes)\n", header->e_shentsize);
-    printf("Number of section headers:         %d\n", header->e_shnum);
-    printf("Section header string table index: %d\n", header->e_shstrndx);
-
-    /* Cleanup and return */
-    free(header);
-    close(fd);
-    return (0);
+	for (index = 0; index < 4; index++)
+	{
+		if (e_ident[index] != 127 &&
+		    e_ident[index] != 'E' &&
+		    e_ident[index] != 'L' &&
+		    e_ident[index] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
 }
+
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,49 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
 
-#define BUFFER_SIZE 1024
+char *create_buffer(char *file);
+void close_file(int fd);
 
-void print_error(const char *message, const char *file);
-char *create_buffer();
-void close_file(int fd, const char *file);
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
 /**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
@@ -15,97 +51,54 @@ void close_file(int fd, const char *file);
  *
  * Return: 0 on success.
  *
-*/
-
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
+ */
 int main(int argc, char *argv[])
 {
-	int from_fd, to_fd, r, w;
+	int from, to, r, w;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		print_error("Usage: cp file_from file_to\n", NULL);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	buffer = create_buffer();
-	from_fd = open(argv[1], O_RDONLY);
-	if (from_fd == -1)
-	{
-		print_error("Error: Can't read from file %s\n", argv[1]);
-	}
-
-	r = read(from_fd, buffer, BUFFER_SIZE);
-	to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (to_fd == -1)
-	{
-		print_error("Error: Can't write to %s\n", argv[2]);
-	}
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from_fd == -1 || r == -1)
+		if (from == -1 || r == -1)
 		{
-			print_error("Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
 		}
 
-		w = write(to_fd, buffer, r);
-		if (to_fd == -1 || w == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
-			print_error("Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
 		}
 
-		r = read(from_fd, buffer, BUFFER_SIZE);
-		to_fd = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
 	free(buffer);
-	close_file(from_fd, argv[1]);
-	close_file(to_fd, argv[2]);
+	close_file(from);
+	close_file(to);
 
 	return (0);
-}
-/**
- * print_error - Prints an error message to stderr.
- * @message: The error message format string.
- * @file: The file name or argument to be included in the error message.
- *
- * Description: Prints the error message specified by the format string.
- *              This function terminates program with an exit status of 1.
- */
-void print_error(const char *message, const char *file)
-{
-	dprintf(STDERR_FILENO, message, file);
-	exit(1);
-}
-
-/**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * file The name of the file buffer is storing chars for.
- *
- * Return: A pointer to the newly-allocated buffer.
- */
-
-char *create_buffer()
-{
-	char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
-
-	if (buffer == NULL)
-{
-		print_error("Error: Can't allocate memory for the buffer\n", NULL);
-	}
-	return (buffer);
-}
-/**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
- * @file: file
- */
-
-void close_file(int fd, const char *file)
-{
-	if (close(fd) == -1)
-	{
-		print_error("Error: Can't close file descriptor for %s\n", file);
-	}
 }
 
