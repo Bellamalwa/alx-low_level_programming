64833fed1a1e308fc44ee37a2bec65cade578fdf
Bellamalwa <duchybella@gmail.com>
2023-05-08 16:23:25 -0700

    resolve

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,48 +1,13 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
+#define BUFFER_SIZE 1024
 
-/**
- * create_buffer - Allocates a buffer of size 1024 bytes.
- * @file: The name of the file associated with the buffer.
- *
- * Return: A pointer to the newly allocated buffer.
- */
-char *create_buffer(char *file)
-{
-	char *buffer;
-
-	buffer = malloc(sizeof(char) * 1024);
-
-	if (buffer == NULL)
-	{
-		fprintf(stderr, "Error: Failed to allocate buffer for %s\n", file);
-		exit(99);
-	}
-
-	return (buffer);
-}
-
-/**
- * close_file - Closes a file descriptor.
- * @fd: The file descriptor to be closed.
- */
-void close_file(int fd)
-{
-	int result;
-
-	result = close(fd);
-
-	if (result == -1)
-	{
-		fprintf(stderr, "Error: Failed to close fd %d\n", fd);
-		exit(100);
-	}
-}
-
+void print_error(const char *message, const char *file);
+char *create_buffer();
+void close_file(int fd, const char *file);
 /**
  * main - Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
@@ -50,53 +15,97 @@ void close_file(int fd)
  *
  * Return: 0 on success.
  *
- * Description: Exits with different error codes in case of errors:
- *              - 97: Incorrect argument count.
- *              - 98: Unable to read from file_from.
- *              - 99: Unable to create or write to file_to.
- *              - 100: Unable to close file descriptors.
- */
+*/
+
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
+	int from_fd, to_fd, r, w;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: cp file_from file_to\n");
-		exit(97);
+		print_error("Usage: cp file_from file_to\n", NULL);
 	}
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buffer();
+	from_fd = open(argv[1], O_RDONLY);
+	if (from_fd == -1)
+	{
+		print_error("Error: Can't read from file %s\n", argv[1]);
+	}
+
+	r = read(from_fd, buffer, BUFFER_SIZE);
+	to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (to_fd == -1)
+	{
+		print_error("Error: Can't write to %s\n", argv[2]);
+	}
 
 	do {
-		if (from == -1 || r == -1)
+		if (from_fd == -1 || r == -1)
 		{
-			fprintf(stderr, "Error: Failed to read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
+			print_error("Error: Can't read from file %s\n", argv[1]);
 		}
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		w = write(to_fd, buffer, r);
+		if (to_fd == -1 || w == -1)
 		{
-			fprintf(stderr, "Error: Failed to write to file %s\n", argv[2]);
-			free(buffer);
-			exit(99);
+			print_error("Error: Can't write to %s\n", argv[2]);
 		}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from_fd, buffer, BUFFER_SIZE);
+		to_fd = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
 	free(buffer);
-	close_file(from);
-	close_file(to);
+	close_file(from_fd, argv[1]);
+	close_file(to_fd, argv[2]);
 
 	return (0);
+}
+/**
+ * print_error - Prints an error message to stderr.
+ * @message: The error message format string.
+ * @file: The file name or argument to be included in the error message.
+ *
+ * Description: Prints the error message specified by the format string.
+ *              This function terminates program with an exit status of 1.
+ */
+void print_error(const char *message, const char *file)
+{
+	dprintf(STDERR_FILENO, message, file);
+	exit(1);
+}
+
+/**
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * file The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+
+char *create_buffer()
+{
+	char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+
+	if (buffer == NULL)
+{
+		print_error("Error: Can't allocate memory for the buffer\n", NULL);
+	}
+	return (buffer);
+}
+/**
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ * @file: file
+ */
+
+void close_file(int fd, const char *file)
+{
+	if (close(fd) == -1)
+	{
+		print_error("Error: Can't close file descriptor for %s\n", file);
+	}
 }
 
